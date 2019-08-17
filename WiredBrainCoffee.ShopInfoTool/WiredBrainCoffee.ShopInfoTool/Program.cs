6737d2ae02e5dd878a605e68af15cba3b53cba98
ealkilicgil <ealkilicgil@gmail.com>
2019-08-17 19:05:25 +0300

    commit exit

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
@@ -8,20 +9,42 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee");
-            Console.WriteLine("Write help to list cofee shop commands");
+            Console.WriteLine("Write help to list coffee shop commands,write quit the exit application ");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
             while (true)
             {
                 var line = Console.ReadLine();
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
-
+                if (string.Equals("exit", line, StringComparison.OrdinalIgnoreCase))
+                    break;
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine(">>>Available coffee shop commands:");
                     foreach (var coffeeShop in coffeeShops)
                     {
                         Console.WriteLine($">" + coffeeShop.Location);
+                    }
+                }
+                else {
+                    var foundCoffeeShops = coffeeShops.
+                        Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+                    if (foundCoffeeShops.Count == 0)
+                    {
+                        Console.WriteLine($">Command '{line}' not found");
+                    }
+                    else if (foundCoffeeShops.Count == 1)
+                    {
+                        var coffeeShop = foundCoffeeShops.Single();
+                        Console.WriteLine($"> Location :{ coffeeShop.Location}");
+                        Console.WriteLine($"> Bean in Stock :{ coffeeShop.BeansInStock}");
+                    }
+                    else {
+                        Console.WriteLine($"> Multiple matching coffee shop commands found:");
+                        foreach (var coffeeType in foundCoffeeShops) {
+                            Console.WriteLine($"> {coffeeType.Location}");
+                        }
                     }
                 }
             }
